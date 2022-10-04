dd2f2cc4f0f369680ce3318e843f78e0150b17e5
Владислав Летучев <kemdizend@gmail.com>
2022-10-04 15:47:45 +0300

    Выполнено

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,14 @@
+let arr1 = [8, 9, 5, 4];
+let arr2 = [8, 9, 5, 4, 8, 3, 5];
+
 function compareArrays(arr1, arr2) {
-  let result;
 
-  // Ваш код
-
-  return result; // boolean
+  return (arr1.length === arr2.length && arr1.every((item, id) => item === arr2[id]));
 }
 
-function advancedFilter(arr) {
-  let resultArr;
 
-  // Ваш код
+let arr3 = [-10,-21,12,123];
 
-  return resultArr; // array
+function advancedFilter(arr3) {
+  return arr3.filter((item) => item > 0).filter((item) => item % 3 === 0).map((item) => item * 10);
 }
