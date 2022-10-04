6fbf9873e857381dd2693d7bdb17f5b9b554e0ef
Владислав Летучев <kemdizend@gmail.com>
2022-10-04 15:46:14 +0300

    Выполнено

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,47 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  
 }
+
+  let student1 = new Student("Борис", "Мужчина", "20");
+  let student2 = new Student("Николай", "Мужчина", "25");
+  let student3 = new Student("Иван", "Мужчина", "30");
+  
+
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+this.subject = subjectName;
+
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+if (this.marks === undefined) { 
+  this.marks = [mark];
+  } else {
+    this.marks.push (mark);
+  }
+}
+
+Student.prototype.addMarks = function (...rest) {
+for (let mark of rest) {
+  this.addMark(mark);
+}
+}
+
+Student.prototype.getAverage = function () {
+  if (this.marks.length === 0) {
+    return 0;
+    }
+   const sum = this.marks.reduce((acc, mark) => acc + mark, 0);
+    return sum / this.marks.length
+}
+
+
+Student.prototype.exclude = function (reason) {
+  
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
